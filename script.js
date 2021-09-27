7b402c811dfea4f9587ec9a556ec2fe9f414fded
sidny <sidny.silva@outlook.com>
2021-09-27 15:50:57 -0300

    definindo primeiras funcaoes e definindo elementos no DOM

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 /*Sidny*/
+const discs = document.querySelectorAll('div');
+const stickerPos = document.querySelectorAll('section')
+let isSelected = false;
+const main = document.querySelector("main")
 
-/*Sidny*/
+console.log(main)
+
+console.log(discs)
+
 
 /*VAGNER*/
 
 /*VAGNER*/
+
+main.addEventListener('click', teste)
+
+function teste(evt) {
+    if (evt.target == stickerPos)
+        console.log(evt.target)
+}
