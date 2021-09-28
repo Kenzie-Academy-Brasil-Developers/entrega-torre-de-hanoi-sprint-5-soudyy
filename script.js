1a8f39bb230dd87dd303df0061621a00ae7f9bd6
sidny <sidny.silva@outlook.com>
2021-09-28 15:18:09 -0300

    Niveis e condições de jogo

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
 /*Sidny*/
+const main = document.querySelector("main");
+const startColumn = document.querySelector('#left');
+const startGame = document.querySelector('#startGame');
+const resetGame = document.querySelector('#reset');
+
 const discs = document.querySelectorAll('div');
-const stickerPos = document.querySelectorAll('section')
-const winningScreen = document.getElementById('winning')
-const playAgain = document.querySelector('button')
+const stickerPos = document.querySelectorAll('section');
+const playAgain = document.querySelectorAll('#winOrLose button');
+const difficulty = document.querySelectorAll('#startGame button');
+const playCounter = document.getElementById('contador')
+const playQuantity = document.createElement('p')
+let playTimes = 1;
 let isSelected = false;
-const main = document.querySelector("main")
+let startValue = 0;
+console.log(resetGame)
+difficulty.forEach(button => button.addEventListener("click", levelSelect))
 
-// console.log(main)
+function levelSelect(evt) {
+    const discColors = ['green', 'red', 'yellow', 'blue', 'purple'];
+    let discQuantity = 0;
+    let diffButton = evt.target;
 
-// console.log(discs)
-
-function winning() {
-    winningScreen.style.display = "flex"
+    diffButton.id == 'facil' ? discQuantity = 3 : diffButton.id == 'medio' ? discQuantity = 4 : discQuantity = 5
+    diffButton.id == 'facil' ? playTimes = 50 : diffButton.id == 'medio' ? playTimes = 30 : playTimes = 50
+    if (diffButton == evt.target) {
+        startGame.style.display = "none";
+        playCounter.style.display = "flex"
+        playQuantity.innerText = playTimes
+        playCounter.appendChild(playQuantity)
+        startValue = playTimes
+        for (let i = discQuantity; i > 0; i--) {
+            const createDiscs = document.createElement('div');
+            createDiscs.id = discColors[i - 1];
+            startColumn.appendChild(createDiscs);
+        }
+    }
 }
 
-playAgain.addEventListener('click', restartGame)
+function endGameCondition() {
+    if (playTimes == 40) {
+        const win = document.getElementById('winning')
+        win.style.display = "flex"
+    } else if (playTimes <= 0) {
+        const lose = document.getElementById('losing')
+        lose.style.display = "flex"
+    }
+}
+endGameCondition()
+resetGame.addEventListener("click", reset)
 
-function restartGame() {
-    winningScreen.style.display = "none"
+function reset() {
+
+    console.log('pegou')
+    playTimes = startValue
+    playQuantity.innerText = playTimes
 }
 /*Sidny*/
+
+
 /*VAGNER*/
 stickerPos.forEach(sticker => sticker.addEventListener("click", handlerClick, true));
 let selectedElement;
